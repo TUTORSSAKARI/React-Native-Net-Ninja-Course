88a6a3185ada3939fe98bb49b64210454c07935d
Sakari Raassina <sakari.raassina@tutors.es>
2025-08-27 09:42:47 +0300

    yesterdays-tasks

---
+++ SHELFIE_APP/app/_layout.jsx
@@ -0,0 +1,28 @@
+import { Stack } from 'expo-router'
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
+import { Colors } from '../constants/Colors'
+
+
+
+const RootLayout = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+  return (
+    <>
+      <StatusBar value="auto" />
+        <Stack screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title
+    }}>
+
+            <Stack.Screen name='index' options={{ title: 'Home' }} />
+            <Stack.Screen name='about' options={{ title: 'About' }} />
+            <Stack.Screen name='contact' options={{ title: 'Contact', headerShown: false }} />            
+        </Stack>
+    </>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
